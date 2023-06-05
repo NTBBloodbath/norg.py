908292b0240312d193767beac27995d7b64729de
vsedov <viv.sedov@hotmail.com>
2023-06-05 15:01:48 +0100

    Feat: Parser - Function that will just prse the information into a GLRParser any conversion is done already.

--- src/norg/parser.py
+++ src/norg/parser.py
@@ -1,5 +1,4 @@
 import parglare
-from attr import attrib, attrs
 from parglare import GLRParser, Grammar
 
 from src.norg.abstract.actions import ConcatAction, IdentityAction, Terminal
@@ -10,18 +9,17 @@ class ParserCore:
     Parser class that performs parsing using the provided grammar, terminals, and actions.
     """
 
-    def __init__(self, grammar, terminals, actions):
+    def __init__(
+        self,
+        grammar,
+    ):
         """
         Initializes the Parser.
 
         Parameters:
         - grammar (str): The grammar specification in parglare format.
-        - terminals (list): List of Terminal objects representing the terminal symbols.
-        - actions (list): List of AbstractAction objects representing the parsing actions.
         """
         self.grammar = grammar
-        self.terminals = terminals
-        self.actions = actions
 
     def parse(self, string):
         """
@@ -50,85 +48,9 @@ class ParserCore:
 
         try:
             trees = parser.parse(string)
-            print(trees)
         except parglare.ParseError as e:
             raise SyntaxError(e)
         assert len(trees) == 1, "Ambiguity detected - got {} parses".format(
             len(trees)
         )
         return trees
-
-
-example_norg_format = """
-* Heading 1
-    Item work content inside
-    ** Heading 2
-        Item work content inside
-        @code Block
-        This is some eaxmple code
-        @end
-""".strip()
-
-grammar = r"""
-document: (Heading| LIST| AT | TEXT)*;
-
-Heading: heading text;
-LIST: list text;
-AT: at_block text at_end;
-TEXT: text;
-
-terminals
-heading: /\*+/;
-list: /\-+/;
-at_block: /@[\w]+/;
-at_end: /@end/;
-text : /[^@\*\-]+/;
-"""
-
-
-document_structure = [
-    ConcatAction("Heading", ["heading", "text"]),
-    ConcatAction("LIST", ["list", "text"]),
-    ConcatAction("AT", ["at_block", "text", "at_end"]),
-    IdentityAction("TEXT", ["text"]),
-]
-
-terminal_patterns = {
-    "heading": r"\*+",
-    "list": r"\-+",
-    "at_block": r"@[\w]+",
-    "at_end": r"@end",
-    "text": r"[^@\*\-]+",
-}
-
-terminals = {
-    name: Terminal(pattern) for name, pattern in terminal_patterns.items()
-}
-
-
-def generate_grammar(document_structure, terminal_patterns):
-    doc_names = " | ".join([action.name for action in document_structure])
-    grammar = f"document: ({doc_names})*;\n\n"
-
-    for action in document_structure:
-        if isinstance(action, ConcatAction):
-            grammar += f"{action.name}: {' '.join(action.elements)};\n"
-        elif isinstance(action, IdentityAction):
-            grammar += f"{action.name}: {action.name.lower()};\n"
-
-    grammar += "\n"
-    grammar += "terminals\n"
-
-    for terminal, pattern in terminal_patterns.items():
-        grammar += f"{terminal}: /{pattern}/;\n"
-
-    return grammar
-
-
-print(generate_grammar(document_structure, terminal_patterns))
-parser = ParserCore(
-    generate_grammar(document_structure, terminal_patterns),
-    terminals,
-    document_structure,
-)
-print(parser.parse(example_norg_format))
